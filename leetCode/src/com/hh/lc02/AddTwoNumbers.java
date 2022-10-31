de0b97f2078414c28433ad33eda300a636d021ef
matrix <matrix@matirx.com>
2022-10-31 19:56:23 +0800

    改版

--- leetCode/src/com/hh/lc02/AddTwoNumbers.java
+++ leetCode/src/com/hh/lc02/AddTwoNumbers.java
@@ -8,8 +8,36 @@ public class AddTwoNumbers {
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode tail = null;
+        int carry = 0;//进位
 
-        return new ListNode();
+        //both is not empty
+        while (l1 != null || l2 != null) {
+            int x = (l1 == null) ? 0 : l1.val;
+            int y = (l2 == null) ? 0 : l2.val;
+
+            //发生进位的时候，下一个数之和要加1 也就是carry位
+            int sum = x + y + carry;
+            //对10取余数，得到个位数
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+
+            //取十位,进位
+            carry = sum / 10; // 16 / 10 = 1
+
+            //move
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+
+        }
+        if (carry > 0) tail.next = new ListNode(carry);
+
+        return head;
     }
 }
 
